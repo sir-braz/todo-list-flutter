87bebc73c87e72cd80a7894ee3a155f10d30ee02
sir-braz <dev.vinicius-batista@hotmail.com>
2025-02-25 17:40:18 -0300

    Este commit implementa o StatefulWidget na tela de detalhes com alteração dinâmica de título e descrição

--- mobile/lib/screens/tela_detalhes.dart
+++ mobile/lib/screens/tela_detalhes.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
 import '../models/item.dart';
 
-class TelaDetalhes extends StatelessWidget {
+class TelaDetalhes extends StatefulWidget {
   final Item item;
 
   TelaDetalhes({required this.item});
 
   @override
+  _TelaDetalhesState createState() => _TelaDetalhesState();
+}
+
+class _TelaDetalhesState extends State<TelaDetalhes> {
+  String? novoTitulo;
+  String? novaDescricao;
+
+  @override
+  void initState() {
+    super.initState();
+    // Inicializa as variáveis com o título e descrição originais
+    novoTitulo = widget.item.titulo;
+    novaDescricao = widget.item.descricao;
+  }
+
+  void _alterarTitulo() {
+    setState(() {
+      novoTitulo = "Título Alterado!";
+    });
+  }
+
+  void _alterarDescricao() {
+    setState(() {
+      novaDescricao = "Descrição alterada pelo usuário.";
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(item.titulo),
+        title: Text(novoTitulo ?? widget.item.titulo),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -18,13 +46,23 @@ class TelaDetalhes extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              item.titulo,
+              novoTitulo ?? widget.item.titulo,
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
             Text(
-              item.descricao,
+              novaDescricao ?? widget.item.descricao,
               style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _alterarTitulo,
+              child: Text("Alterar Título"),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _alterarDescricao,
+              child: Text("Alterar Descrição"),
             ),
           ],
         ),
